0e0d4840a3b91626a444a54c26122dc5dba66a2c
Lucas <lucas@agtlucas.com>
2015-06-06 22:17:14 -0300

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins');
 
 gulp.task('js', function() {
-  gulp.src('./app.jsx')
+  gulp.src('assets/js/app.jsx')
     .pipe($.webpack(require('./webpack.config.js')))
     .pipe(gulp.dest('build'));
 });
