1c01a10c7138059aa9c015a2610bfa9aa70d466b
Lucas <lucas@agtlucas.com>
2015-06-06 22:32:02 -0300

    Update app.jsx

--- assets/js/app.jsx
+++ assets/js/app.jsx
@@ -1,19 +1,27 @@
 import React from 'react/addons';
 
-export default class App extends React.Component {
-  // ES6 constructor
-  // constructor(props) {
-  //   super(props);
-  // }
-  //
-  // // Initial State
-  // getInitialState() {
-  //
-  // }
+class ContentToggle extends React.Component {
+  render() {
+    return(
+      <div>
+        <div>{this.props.summary}</div>
+        <div>{this.props.children}</div>
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
 
   render() {
     return(
-      <div>App!</div>
+      <div>
+        <ContentToggle summary="Pizza!!!">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </ContentToggle>
+      </div>
     )
   }
 }
