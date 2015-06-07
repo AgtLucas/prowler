f2f7664d4ed6d9c0f2835fed6cd2a6548cadac83
Lucas <lucas@agtlucas.com>
2015-06-06 21:50:08 -0300

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -1,7 +1,19 @@
 import React from 'react/addons';
 
 export default App extends React.Component {
+  // ES6 constructor
   constructor(props) {
     super(props);
+  }
+
+  // Initial State
+  getInitialState() {
+
+  }
+
+  render() {
+    return(
+      <div>App!</div>
+    )
   }
 }
